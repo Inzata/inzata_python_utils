1550eecbb01f28f3e6f21bc1ada902a7ed5816c6
Ales Cervenka <ales.cervenka@inzata.com>
2021-04-15 15:53:07 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='inzata_python_utils',
+      version='0.5',
+      description='InZata Python utility libraries',
+      packages=find_packages(),
+      install_requires=[],
+      zip_safe=False)
